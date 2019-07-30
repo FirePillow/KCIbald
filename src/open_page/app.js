dfdc9b44b4d8027165a1c1b68a5d74e2dfae9c24
wusatosi <hi@wusatosi.com>
2019-07-30 19:19:08 +0800

    fix typo in comment

--- src/open_page/app.js
+++ src/open_page/app.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import app from './app.vue';
 
 // Have to initialize vue this way,
-// since we're using single file synatx
+// since we're using single file syntax
 new Vue({
     render: h => h(app),
 }).$mount('#app');
