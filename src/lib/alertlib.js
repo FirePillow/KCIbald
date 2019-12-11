977066412d150748ea4fc75af57ee8a9aef18da6
Mike Lu <heatmint2333@gmail.com>
2019-12-11 08:07:56 +0800

    Alert lib installation

---
+++ src/lib/alertlib.js
@@ -0,0 +1,37 @@
+require("../css/alertlib.css");
+
+const layer = require("layui-layer");
+const exp ={};
+
+layer.config({
+    path: '/node_modules/layui-layer/src/'
+});
+
+function user_err(info){
+    layer.msg(info, {
+        offset: 't',
+        anim: 1,
+        time:5000000,
+        //time for develop environment
+        skin: 'alertlib'
+    });
+}
+
+function unexpected_err(info){
+    layer.alert(info, {icon: 2});
+}
+
+function twoinferr(title,first,second){
+    var content = `<p style="font-size:1.5em;"><b>${first}</b></p>`;
+    content += `<p>${second}</p>`;
+    layer.open({
+        title:title,
+        content:content
+    })
+}
+
+exp.twoinferr = twoinferr;
+exp.user_err = user_err;
+exp.unexpected_err = unexpected_err;
+
+module.exports = exp;
