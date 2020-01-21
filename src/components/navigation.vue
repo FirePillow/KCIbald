aa21971f5c231e3b2dcf778f7ea8db2d0b2dde75
Mike Lu <heatmint2333@gmail.com>
2020-01-21 17:02:19 +0800

    Create navigation.vue

---
+++ src/components/navigation.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="searchheader">
+        <div id="insidesearch">
+            <img id="bald" src="../images/logo.svg" alt="KCIBald!" />
+            <div id="searchright">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
+            </div>
+            <div id="search">
+                搜索框开发中。。。
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "navigation",
+        mounted() {
+            console.log('navigation bar mounted.')
+        }
+    }
+</script>
+
+<style scoped>
+    body {
+        margin: 0px;
+        background-color: #F5F5F5;
+    }
+
+    #searchheader {
+        font-size: 1.4em;
+        font-family: Arial;
+        background-color: #FFF;
+        box-shadow: 0 1px 3px rgba(26, 26, 26, .1);
+        overflow: hidden;
+    }
+
+    #insidesearch {
+        margin-left: 15%;
+        margin-right: 15%;
+        margin-top: 5px;
+        margin-bottom: 1.5em;
+    }
+
+    #searchright {
+        float: right;
+    }
+
+    #search {
+        display: inline;
+        background-color: #FFE505;
+        border-radius: 500px;
+        float: right;
+        padding-left: 10px;
+        padding-right: 10px;
+        margin-right: 10%;
+    }
+
+    #insidesearch #bald {
+        max-width: 3em;
+        float: left;
+    }
+</style>
