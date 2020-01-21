105097cce0f44e21d8110e98ecf6d394e3989490
Mike Lu <heatmint2333@gmail.com>
2020-01-21 17:03:28 +0800

    Create openpage(withbug)

---
+++ src/components/open_page.vue
@@ -0,0 +1,18 @@
+<template>
+    <navigation></navigation>
+</template>
+
+<script>
+    import navigation from '../components/navigation.vue';
+    export default {
+        name: "open_page",
+        components: {navigation},
+        mounted() {
+            console.log('openpage mounted.')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/index/app.js
+++ src/index/app.js
@@ -1,1 +1,23 @@
-'glue';
+//require('./style.css');
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import open_page from '../components/open_page.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+    { path: '', component: open_page }
+//    { path: '/posts/r/:regionKey/p/:postKey', component: posts }
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+});
+
+// Have to initialize vue this way,
+// since we're using single file syntax
+new Vue({
+    router,
+    render: h => h(app),
+}).$mount('#app');
